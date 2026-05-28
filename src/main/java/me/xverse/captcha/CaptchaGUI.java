ad5107954dded866916bf0a6c8ecf9bd726eedad
MrXlson <dominikdrasar39@gmail.com>
2026-05-28 23:07:59 +0200

    CaptchaGUI.java

---
+++ src/main/java/me/xverse/captcha/CaptchaGUI.java
@@ -0,0 +1,50 @@
+package me.xverse.captcha;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
+
+public class CaptchaGUI {
+
+    public static void openCaptcha(Player p) {
+
+        FileConfiguration config = Main.getInstance().getConfig();
+
+        int size = config.getInt("captcha.gui.size");
+
+        String title = config.getString("captcha.gui.title")
+                .replace("&", "§");
+
+        Inventory inv = Bukkit.createInventory(null, size, title);
+
+        Random random = new Random();
+
+        int slot = random.nextInt(size);
+
+        Material fill = Material.valueOf(
+                config.getString("captcha.gui.fill-material")
+        );
+
+        Material captcha = Material.valueOf(
+                config.getString("captcha.gui.captcha-material")
+        );
+
+        for(int i = 0; i < size; i++) {
+
+            if(i == slot) {
+                inv.setItem(i, new ItemStack(captcha));
+            } else {
+                inv.setItem(i, new ItemStack(fill));
+            }
+        }
+
+        CaptchaListener.captchaPlayers.add(p.getUniqueId());
+
+        p.openInventory(inv);
+    }
+}
