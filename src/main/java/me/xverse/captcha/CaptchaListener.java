28c90c2c387b12cbdf621d7bb2ccf51d0ef5feb0
MrXlson <dominikdrasar39@gmail.com>
2026-05-28 23:08:40 +0200

    CaptchaListener.java

---
+++ src/main/java/me/xverse/captcha/CaptchaListener.java
@@ -0,0 +1,83 @@
+package me.xverse.captcha;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class CaptchaListener implements Listener {
+
+    public static Set<UUID> captchaPlayers = new HashSet<>();
+
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+
+        if(!(e.getWhoClicked() instanceof Player p)) return;
+
+        FileConfiguration config = Main.getInstance().getConfig();
+
+        String title = config.getString("captcha.gui.title")
+                .replace("&", "§");
+
+        if(!e.getView().getTitle().equals(title)) return;
+
+        e.setCancelled(true);
+
+        if(e.getCurrentItem() == null) return;
+
+        Material captcha = Material.valueOf(
+                config.getString("captcha.gui.captcha-material")
+        );
+
+        if(e.getCurrentItem().getType() == captcha) {
+
+            captchaPlayers.remove(p.getUniqueId());
+
+            p.closeInventory();
+
+            p.sendMessage(
+                    config.getString("messages.success")
+                            .replace("&", "§")
+            );
+
+        } else {
+
+            Bukkit.getScheduler().runTaskLater(
+                    Main.getInstance(),
+                    () -> p.kickPlayer(
+                            config.getString("messages.fail")
+                                    .replace("&", "§")
+                    ),
+                    1L
+            );
+        }
+    }
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent e) {
+
+        Player p = (Player) e.getPlayer();
+
+        if(captchaPlayers.contains(p.getUniqueId())) {
+
+            FileConfiguration config = Main.getInstance().getConfig();
+
+            Bukkit.getScheduler().runTaskLater(
+                    Main.getInstance(),
+                    () -> p.kickPlayer(
+                            config.getString("messages.close")
+                                    .replace("&", "§")
+                    ),
+                    1L
+            );
+        }
+    }
+}
